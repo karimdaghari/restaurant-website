122ce3d2de6731ce1897a0132dab7185c9ca3729
Karim Daghari <karimdxy@gmail.com>
2019-09-24 20:21:36 +0100

    feat(component): add navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,30 @@
 <template>
-  <div>
-    <p>Navbar here</p>
+  <div class="d-flex justify-content-around">
+    <div></div>
+    <b-navbar>
+      <b-nav>
+        <g-link class="nav-link" to="/#about">About</g-link>
+        <g-link class="nav-link" to="/#menu">Menu</g-link>
+      </b-nav>
+      <b-navbar-brand href="/">
+        <img
+          src="http://www.pngall.com/wp-content/uploads/2016/04/Hexagon-PNG.png"
+          alt="Logo"
+          class="logo"
+        />
+      </b-navbar-brand>
+      <b-nav class="ml-n4">
+        <b-nav-item router-tag="a" href="https://facebook.com/">
+          <unicon name="facebook-f"></unicon>
+        </b-nav-item>
+        <b-nav-item router-tag="a" href="https://instagram.com/">
+          <unicon name="instagram"></unicon>
+        </b-nav-item>
+      </b-nav>
+    </b-navbar>
+    <div class="d-flex align-items-center">
+      <b-button variant="outline-danger" size="md">CTA</b-button>
+    </div>
   </div>
 </template>
 
@@ -8,5 +32,12 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss" scoped>
+.logo {
+  width: 6.5em;
+}
+button {
+  margin-right: 1em;
+}
 </style>
+
--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@
 import DefaultLayout from '~/layouts/Default.vue';
 import BootstrapVue from 'bootstrap-vue';
 import '@/assets/style/custom.scss';
+import Unicon from 'vue-unicons';
+import { uniFacebookF, uniInstagram } from 'vue-unicons/src/icons';
 
 export default function(Vue, { router, head, isClient }) {
+  const icons = [uniFacebookF, uniInstagram];
   // Set default layout as a global component
   Vue.component('Layout', DefaultLayout);
   Vue.use(BootstrapVue);
+  Unicon.add(icons);
+  Vue.use(Unicon);
 }

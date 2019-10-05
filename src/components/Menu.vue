f5d94d8ccf8645b33cc3f294df4086eaab6f86bf
Karim Daghari <karimdxy@gmail.com>
2019-10-05 21:48:45 +0100

    refactor: add looping

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -3,56 +3,26 @@
     <b-heading>Menu</b-heading>
     <b-container>
       <h3 class="text-uppercase mb-2">Entrées</h3>
-      <b-row class="mb-2">
-        <b-col md="4" class="mb-2">
+      <b-row class="mb-2 ml-1">
+        <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
           <menu-single-item>
-            <template #mealName>Entrée 1</template>
-          </menu-single-item>
-        </b-col>
-        <b-col md="4" class="mb-2">
-          <menu-single-item>
-            <template #mealName>Entrée 2</template>
-          </menu-single-item>
-        </b-col>
-        <b-col md="4" class="mb-2">
-          <menu-single-item>
-            <template #mealName>Entrée 3</template>
+            <template #mealName>Entrée {{ n }}</template>
           </menu-single-item>
         </b-col>
       </b-row>
       <h3 class="text-uppercase mb-2">Plats</h3>
-      <b-row class="mb-2">
-        <b-col md="4" class="mb-2">
+      <b-row class="mb-2 ml-1">
+        <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
           <menu-single-item>
-            <template #mealName>Plat 1</template>
-          </menu-single-item>
-        </b-col>
-        <b-col md="4" class="mb-2">
-          <menu-single-item>
-            <template #mealName>Plat 2</template>
-          </menu-single-item>
-        </b-col>
-        <b-col md="4" class="mb-2">
-          <menu-single-item>
-            <template #mealName>Plat 3</template>
+            <template #mealName>Plat {{ n }}</template>
           </menu-single-item>
         </b-col>
       </b-row>
       <h3 class="text-uppercase mb-2">Déserts</h3>
-      <b-row class="mb-2">
-        <b-col md="4" class="mb-2">
+      <b-row class="mb-2 ml-1">
+        <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
           <menu-single-item>
-            <template #mealName>Desert 1</template>
-          </menu-single-item>
-        </b-col>
-        <b-col md="4" class="mb-2">
-          <menu-single-item>
-            <template #mealName>Desert 2</template>
-          </menu-single-item>
-        </b-col>
-        <b-col md="4" class="mb-2">
-          <menu-single-item>
-            <template #mealName>Desert 3</template>
+            <template #mealName>Désert {{ n }}</template>
           </menu-single-item>
         </b-col>
       </b-row>

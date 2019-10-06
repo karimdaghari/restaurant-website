7e3db40b637d0ecd7443cb73e96efb4c472e652d
Karim Daghari <karimdxy@gmail.com>
2019-10-06 14:27:23 +0100

    style: use heading, add structure & a see more btn

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,31 +1,48 @@
 <template>
   <section id="menu">
-    <b-heading>Menu</b-heading>
     <b-container>
-      <h3 class="text-uppercase mb-2">Entrées</h3>
-      <b-row class="mb-2">
-        <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
-          <menu-single-item>
-            <template #mealName>Entrée {{ n }}</template>
-          </menu-single-item>
-        </b-col>
-      </b-row>
-      <h3 class="text-uppercase mb-2">Plats</h3>
-      <b-row class="mb-2">
-        <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
-          <menu-single-item>
-            <template #mealName>Plat {{ n }}</template>
-          </menu-single-item>
-        </b-col>
-      </b-row>
-      <h3 class="text-uppercase mb-2">Déserts</h3>
-      <b-row class="mb-2">
-        <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
-          <menu-single-item>
-            <template #mealName>Désert {{ n }}</template>
-          </menu-single-item>
-        </b-col>
-      </b-row>
+      <b-heading>Menu</b-heading>
+      <section>
+        <b-heading>Entrées</b-heading>
+        <b-row class="mb-2">
+          <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
+            <menu-single-item>
+              <template #mealName>Entrée {{ n }}</template>
+            </menu-single-item>
+          </b-col>
+        </b-row>
+        <b-row class="d-flex justify-content-center">
+          <b-button pill size="md" variant="outline-info" href="#">Consulter nos entrées</b-button>
+        </b-row>
+      </section>
+
+      <section>
+        <b-heading>Plats</b-heading>
+        <b-row class="mb-2">
+          <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
+            <menu-single-item>
+              <template #mealName>Plat {{ n }}</template>
+            </menu-single-item>
+          </b-col>
+        </b-row>
+        <b-row class="d-flex justify-content-center">
+          <b-button pill size="md" variant="outline-info" href="#">Consulter nos plats</b-button>
+        </b-row>
+      </section>
+
+      <section>
+        <b-heading>Déserts</b-heading>
+        <b-row class="mb-2">
+          <b-col md="4" class="mb-2" v-for="n in 3" :key="n">
+            <menu-single-item>
+              <template #mealName>Désert {{ n }}</template>
+            </menu-single-item>
+          </b-col>
+        </b-row>
+        <b-row class="d-flex justify-content-center">
+          <b-button pill size="md" variant="outline-info" href="#">Consulter nos déserts</b-button>
+        </b-row>
+      </section>
     </b-container>
   </section>
 </template>
@@ -41,5 +58,9 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+a {
+  text-transform: uppercase;
+  margin-bottom: 1em;
+}
 </style>
